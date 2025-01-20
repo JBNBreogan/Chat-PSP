a2f3f23d147a8844350b00f18f7cc474ee796a22
Nicolás <nicolaigl72@gmail.com>
2025-01-20 19:02:44 +0100

    HiloClientes

--- Chat/src/Servidor/HiloCliente.java
+++ Chat/src/Servidor/HiloCliente.java
@@ -47,19 +47,78 @@ public class HiloCliente extends Thread{
         String tipo=lista.get(0);
         switch (tipo) {
             case "SOLICITUD_CONEXION":
-               
+                confirmConnection(lista.get(1));
                 break;
             case "SOLICITUD_DESCONEXION":
-               
+                confirmarDesconnection();
                 break;                
             case "MENSAJE":
-            
+                String destinatario=lista.get(2);
+                server.clientes.stream().filter(h -> (destinatario.equals(h.getIde()))).forEach((h) -> h.sendMessage(lista));
                 break;
             default:
                 break;
         }
     }
 
-   
+    private void confirmConnection(String identificador) {
+        Servidor.idClientes++;
+        this.id=Servidor.idClientes+" - "+id;
+        LinkedList<String> lista=new LinkedList<>();
+        lista.add("CONEXION_ACEPTADA");
+        lista.add(this.id);
+        lista.addAll(server.getUsuarios());
+        sendMessage(lista);
+        server.addLog("\nNuevo cliente: "+this.id);
+        LinkedList<String> auxLista=new LinkedList<>();
+        auxLista.add("NUEVO_USUARIO_CONECTADO");
+        auxLista.add(this.id);
+        server.clientes
+                .stream()
+                .forEach(cliente -> cliente.sendMessage(auxLista));
+        server.clientes.add(this);
+    }
+
+
+    private void confirmarDesconnection() {
+        LinkedList<String> auxLista=new LinkedList<>();
+        auxLista.add("USUARIO_DESCONECTADO");
+        auxLista.add(this.id);
+        server.addLog("\nEl cliente \""+this.id+"\" se ha desconectado.");
+        this.desconnect();
+        for(int i=0;i<server.clientes.size();i++){
+            if(server.clientes.get(i).equals(this)){
+                server.clientes.remove(i);
+                break;
+            }
+        }
+        server.clientes.stream().forEach(h -> h.sendMessage(auxLista));        
+    }
+
+    private void sendMessage(LinkedList<String> lista){
+        try {
+            oos.writeObject(lista);            
+        } catch (IOException e) {
+            System.err.println("Error al enviar el objeto al cliente.");
+        }
+    }    
+
+    public void desconnect() {
+        try {
+            socket.close();
+            preparado=false;
+        } catch (IOException ex) {
+            System.err.println("Error al cerrar el socket de comunicación con el cliente.");
+        }
+    }
+
+    public void run() {				
+        try{
+           listen();
+        } catch (Exception ex) {
+            System.err.println("Error al llamar al método readLine del hilo del cliente.");
+        }
+        desconnect();
+    }
 }
 
